f0c42634d1c9f5d54873c83ca93dec37b9ac9d10
TeliStaffs <97461050+User083@users.noreply.github.com>
2023-06-28 10:15:10 +0100

    Initial Setup
    - Adds Tailwind & Axios
    - Cleans up files
    - Adds redirects

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js, jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+    
+      boxShadow: {
+      card: "0px 35px 120px -15px #211e35",
+    },
+    screens: {
+      xs: "450px",
+    },},
+  },
+  plugins: [],
+}
+
